7fb17d9a3bcddcafb240d2288f10083e963bad51
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2023-11-25 13:36:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
+import tkinter as tk
+from tkinter import messagebox, simpledialog
 
 
 def play_again():
-    play_again = input("Do you want to play again? (yes/no) ").lower()
-    return play_again.startswith('y')
+    answer = messagebox.askyesno("Play Again", "Do you want to play again?")
+    return answer
 
 
 def choose_difficulty():
@@ -13,73 +15,78 @@ def choose_difficulty():
         'hard': (1, 200)
     }
 
-    print("Choose a difficulty level:")
+    choice = None
+
+    def set_difficulty(diff):
+        nonlocal choice
+        choice = diff
+        window.destroy()
+
+    window = tk.Tk()
+    window.title("Choose Difficulty")
+
     for level, (low, high) in difficulties.items():
-        print(f"{level.capitalize()} ({low}-{high})")
-    
-    while True:
-        choice = input('Enter your choice of difficulty: ').strip().lower()
-        if choice in difficulties:
-            return difficulties[choice]
-        print('Invalid choice. Please try again.')
+        btn = tk.Button(window, text=f"{level.capitalize()} ({low}-{high})", command=lambda l=low, h=high: set_difficulty((l, h)))
+        btn.pack(pady=5)
+
+    window.mainloop()
+
+    return choice
 
 
 def take_guess(low, high):
-    while True:
-        guess = input(f'Enter your guess ({low}-{high}): ')
-        if guess.isdecimal():
-            guess = int(guess)
-            if low <= guess <= high:
-                return guess
-        print(f'Please enter a valid number between {low} and {high}.')
+    guess = tk.simpledialog.askinteger("Guess", f"Enter your guess ({low}-{high}):", minvalue=low, maxvalue=high)
+    return guess
+
 
 def display_welcome_message():
-    print(f'Hello, {myName}! Welcome to the Guessing Game!')
+    messagebox.showinfo("Welcome", f"Hello, {myName}! Welcome to the Guessing Game!")
+
 
 def display_result_message(guess_taken):
     if guess_taken == 1:
-        print(f'Wow, {myName}! You guessed my number in 1 try! You\'re a genius!')
+        messagebox.showinfo("Congratulations!", f'Wow, {myName}! You guessed my number in 1 try! You\'re a genius!')
     else:
-         print(f'Good job, {myName}! You guessed my number in {guess_taken} tries!')
+        messagebox.showinfo("Congratulations!", f'Good job, {myName}! You guessed my number in {guess_taken} tries!')
+
 
 def main_game_loop():
     score = 10
     while True:
-        low, high = choose_difficulty()
+        difficulty = choose_difficulty()
+        low, high = difficulty
         secret_number = random.randint(low, high)
 
-        print(f'I am thinking of a number between {low} and {high}')
+        messagebox.showinfo("Game Start", f'I am thinking of a number between {low} and {high}')
 
         for guess_taken in range(10):
-            print(f'You have {10 - guess_taken} guesses left.')
+            remaining_guesses = 10 - guess_taken
             guess = take_guess(low, high)
 
             if guess == secret_number:
                 break
             elif guess < secret_number:
-                print('Your guess is too low.')
+                messagebox.showinfo("Hint", 'Your guess is too low.')
             else:
-                print('Your guess is too high')
+                messagebox.showinfo("Hint", 'Your guess is too high')
 
         else:
-            print(f'Game Over! The number I was thinking of was {secret_number}.')
+            messagebox.showinfo("Game Over", f'Game Over! The number I was thinking of was {secret_number}.')
 
         if guess == secret_number and guess_taken < score:
-            display_result_message(guess_taken+1)
-            score -= (guess_taken+1)
+            display_result_message(guess_taken + 1)
+            score -= (guess_taken + 1)
         else:
-            print(f'Game Over! The number I was thinking of was {secret_number}.')  
+            messagebox.showinfo("Game Over", f'Game Over! The number I was thinking of was {secret_number}.')
 
-        print(f'Your Score: {score}')
+        messagebox.showinfo("Your Score", f'Your Score: {score}')
 
         if not play_again():
-            print(f'Thank you for playing, {myName}!')
+            messagebox.showinfo("Goodbye", f'Thank you for playing, {myName}!')
             break
 
 
-# Entry Point
-print('Hello! What is your name?')
-myName = input('>')
+myName = tk.simpledialog.askstring("Name", "Hello! What is your name?")
 myName = myName.title()
 display_welcome_message()
 main_game_loop()
