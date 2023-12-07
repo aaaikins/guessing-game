0b01638fdd5465e63c746577de5d201f8211517c
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2023-12-07 18:56:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import random
 import tkinter as tk
 from tkinter import messagebox, simpledialog
 
-
 def play_again():
+    """Ask the user if they want to play again and return the answer."""
     answer = messagebox.askyesno("Play Again", "Do you want to play again?")
     return answer
 
-
 def choose_difficulty():
+    """Display a window to allow the user to choose the difficulty level.
+    
+    Returns:
+        tuple: A tuple representing the chosen difficulty level range.
+    """
     difficulties = {
         'easy': (1, 50),
         'medium': (1, 100),
@@ -18,6 +22,7 @@ def choose_difficulty():
     choice = None
 
     def set_difficulty(diff):
+        """Set the chosen difficulty level."""
         nonlocal choice
         choice = diff
         window.destroy()
@@ -33,24 +38,36 @@ def choose_difficulty():
 
     return choice
 
-
 def take_guess(low, high):
+    """Prompt the user to input a guess within the given range.
+    
+    Args:
+        low (int): The lower bound of the valid guess range.
+        high (int): The upper bound of the valid guess range.
+    
+    Returns:
+        int: The user's guess.
+    """
     guess = tk.simpledialog.askinteger("Guess", f"Enter your guess ({low}-{high}):", minvalue=low, maxvalue=high)
     return guess
 
-
 def display_welcome_message():
+    """Display a welcome message to the user."""
     messagebox.showinfo("Welcome", f"Hello, {myName}! Welcome to the Guessing Game!")
 
-
 def display_result_message(guess_taken):
+    """Display a message based on the user's performance.
+    
+    Args:
+        guess_taken (int): The number of guesses the user took to find the correct answer.
+    """
     if guess_taken == 1:
         messagebox.showinfo("Congratulations!", f'Wow, {myName}! You guessed my number in 1 try! You\'re a genius!')
     else:
         messagebox.showinfo("Congratulations!", f'Good job, {myName}! You guessed my number in {guess_taken} tries!')
 
-
 def main_game_loop():
+    """Run the main game loop."""
     score = 10
     while True:
         difficulty = choose_difficulty()
@@ -84,7 +101,6 @@ def main_game_loop():
         if not play_again():
             messagebox.showinfo("Goodbye", f'Thank you for playing, {myName}!')
             break
-
 
 myName = tk.simpledialog.askstring("Name", "Hello! What is your name?")
 myName = myName.title()
